fa6cf07b35a36dc398df0a51754b8fde6b5dda96
Kullike Annuk <kullike.annuk@voco.ee>
2025-02-07 11:21:41 +0200

    Feature-c added survey

--- welcome.py
+++ welcome.py
@@ -1,4 +1,6 @@
 form get_name import *
+form validate_age import *
+form survaey import *
 
 print("="*35)
 print("Welcome to voting eligilibity check")
@@ -8,3 +10,13 @@ print("/n")
 
 user_name = get_namme()
 print(f"Hello {user_name}!\n")
+
+age = int(input("Enter your age:"))
+validate_age(age)
+
+survaey_flag = input("Would you like to participate in our survey (y/n):")
+
+if survaey_flag == 'Y':
+    survaey()
+
+    print("\nThank you! Please visit again.\n\n")
